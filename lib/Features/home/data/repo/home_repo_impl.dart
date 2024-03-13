b51c9f343d19b03de827df020a8f9e0e307ad4b9
MuhanadIbrahim <parhoegypt@gmail.com>
2024-03-13 02:03:34 +0200

    implement newset books

--- lib/Features/home/data/repo/home_repo_impl.dart
+++ lib/Features/home/data/repo/home_repo_impl.dart
@@ -1,13 +1,33 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dartz/dartz.dart';
+
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/data/repo/home_repo.dart';
 import 'package:bookly_app/core/errors/failures.dart';
-import 'package:dartz/dartz.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 
-class HomeRepoImpl extends HomeRepo{
+class HomeRepoImpl extends HomeRepo {
+  ApiService apiService;
+
+  HomeRepoImpl({
+    required this.apiService,
+  });
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewsetBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:Novels');
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(item);
+      }
+
+      return right(books);
+    } on Exception catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
